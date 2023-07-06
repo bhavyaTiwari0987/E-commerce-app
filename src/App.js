cacfd92861a9c09b9482f595893e7ed74eda0f19
Bhavya Tiwari <bhavyatiwari917@gmail.com>
2023-07-06 22:53:06 +0530

    Router setup done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import Home from './pages/Home';
+import Cart from './pages/Cart';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+  ScrollRestoration
+} from 'react-router-dom';
+
+const Layout = () => {
+  return (
+    <div>
+      <Header/>
+      <Outlet/>
+      <Footer/>
+    </div>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children: [
+      {
+        path:"/",
+        element: <Home/>
+      },
+      {
+        path:"/cart",
+        element: <Cart/>,
+      }
+    ]
+  }
+])
 
 function App() {
   return (
     <div className="font-bodyFont">
-      <Header/>
-      <Home/>
-      <Footer/>
+     <RouterProvider router={router}/>
     </div>
   );
 }
